b451e6ed22c4ed60e8b012d28853fd3d67c5ddb7
campique <info@campique.com>
2024-11-08 20:09:59 +0100

    Create theme.js

---
+++ client/src/styles/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FE6B8B',
+    },
+    secondary: {
+      main: '#FF8E53',
+    },
+  },
+  typography: {
+    fontFamily: '"Comic Sans MS", cursive, sans-serif',
+  },
+});
+
+export default theme;
